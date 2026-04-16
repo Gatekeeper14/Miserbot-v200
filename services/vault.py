10462eaf316a09d72de0e9e67fcf5205263cb9d4
Gatekeeper14 <Parapsychologenius@yahoo.com>
2026-04-16 18:55:55 -0400

    Update vault.py

--- services/vault.py
+++ services/vault.py
@@ -1,23 +1,40 @@
-from database import get_conn,release_conn
+from database import get_conn, release_conn
+
 
 def unlock_vault(user):
 
-    conn=get_conn()
-    cur=conn.cursor()
+    conn = get_conn()
+    cur = conn.cursor()
 
     cur.execute("""
     CREATE TABLE IF NOT EXISTS vault(
         user_id BIGINT PRIMARY KEY,
-        unlocked BOOLEAN
+        unlocked BOOLEAN DEFAULT FALSE,
+        super_vault BOOLEAN DEFAULT FALSE
     )
     """)
 
     cur.execute("""
-    INSERT INTO vault(user_id,unlocked)
-    VALUES(%s,TRUE)
+    INSERT INTO vault(user_id, unlocked)
+    VALUES(%s, TRUE)
     ON CONFLICT(user_id)
     DO UPDATE SET unlocked=TRUE
-    """,(user,))
+    """, (user,))
+
+    conn.commit()
+    release_conn(conn)
+
+
+def unlock_super_vault(user):
+
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute("""
+    UPDATE vault
+    SET super_vault = TRUE
+    WHERE user_id = %s
+    """, (user,))
 
     conn.commit()
     release_conn(conn)
