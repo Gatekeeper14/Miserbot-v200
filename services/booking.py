0d37c1d1436bccfcf35f793bbfe523e66c2e5e78
Gatekeeper14 <Parapsychologenius@yahoo.com>
2026-04-16 17:54:15 -0400

    Create booking.py

---
+++ services/booking.py
@@ -0,0 +1,14 @@
+BOOKING_PACKAGES={
+"feature":5000,
+"studio_bundle":1200,
+"video_cameo":1200,
+"small_club":2500,
+"medium_club":5000,
+"large_club":15000,
+"fan_photo":50,
+"backstage":250,
+"donation":1
+}
+
+def get_packages():
+    return BOOKING_PACKAGES
