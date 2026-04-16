bc51427be84f3dc78baac4e5c2a6b5ae73bda60e
Gatekeeper14 <Parapsychologenius@yahoo.com>
2026-04-16 19:08:21 -0400

    Create radio.py

---
+++ services/radio.py
@@ -0,0 +1,8 @@
+RADIO_LINK = "https://t.me/bazragodradio"
+
+def get_radio_message():
+    return (
+        "📻 BazraGod Radio\n\n"
+        "Streaming 24/7 from Parish 14.\n\n"
+        f"Listen here:\n{RADIO_LINK}"
+    )
