d7d6562d16fad2f21d1d3b36e5a44af59fdd8cf4
Gatekeeper14 <Parapsychologenius@yahoo.com>
2026-04-16 18:03:07 -0400

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,24 +1,17 @@
-from telegram import ReplyKeyboardMarkup
-from config import INTRO_FILE_ID,FIRST_MESSAGE_FILE_ID
+async def start(update, context):
 
-menu=ReplyKeyboardMarkup(
-[
-["🎧 Music","🛒 Store"],
-["💰 MiserCoins","🏆 Leaderboard"],
-["💬 Lounge","⚙️ Support"]
-],
-resize_keyboard=True
-)
+    if INTRO_FILE_ID and INTRO_FILE_ID.strip() != "":
+        try:
+            await update.message.reply_voice(INTRO_FILE_ID)
+        except:
+            pass
 
-async def start(update,context):
-
-    if INTRO_FILE_ID:
-        await update.message.reply_voice(INTRO_FILE_ID)
-
-    if FIRST_MESSAGE_FILE_ID:
-        await update.message.reply_voice(FIRST_MESSAGE_FILE_ID)
+    if FIRST_MESSAGE_FILE_ID and FIRST_MESSAGE_FILE_ID.strip() != "":
+        try:
+            await update.message.reply_voice(FIRST_MESSAGE_FILE_ID)
+        except:
+            pass
 
     await update.message.reply_text(
-        "Welcome to Miserbot ecosystem.",
-        reply_markup=menu
+        "Welcome to Miserbot ecosystem."
     )
